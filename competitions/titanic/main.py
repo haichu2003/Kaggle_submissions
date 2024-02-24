a96a42d824cb1a5abe17b07e564d1a3fe0e82737
Chu Hai <duchaichu1303@gmail.com>
2024-02-24 08:51:35 +0200

    update notebook add random forest classifier and pipeline update main.py

--- competitions/titanic/main.py
+++ competitions/titanic/main.py
@@ -16,8 +16,8 @@ def load_data(file):
     return df
 
 # preprocess data
-# exclusive for this data only
-def preprocess(df):
+# exclusive for these data only
+def preprocess_train_data(df):
     data = df.drop(['PassengerId',
                 'Name',
                 'Ticket',
@@ -50,63 +50,94 @@ def preprocess(df):
     X_transformed = preprocessor.fit_transform(X)
     return X_transformed, y
 
+def preprocess_test_data(df):
+    test_data_used = df.loc[:,['Pclass', 'Sex', 'Age', 'Fare', 'Embarked']]
+
+    # fill NaN values in test data
+    test_data_used['Age'] = test_data_used['Age'].fillna(test_data_used['Age'].mean())
+    test_data_used['Fare'] = test_data_used['Fare'].fillna(test_data_used['Fare'].mean())
+
+    # scale numeric features
+    numeric_features = ['Age', 'Fare']
+    numeric_transformer = Pipeline(
+        steps=[('scaler', StandardScaler())]
+    )
+
+    # one hot encode categorical features
+    categorical_features = ['Pclass', 'Sex', 'Embarked']
+    categorical_transformer = Pipeline(
+        steps=[("encoder", OneHotEncoder(handle_unknown='ignore'))]
+    )
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, numeric_features),
+            ("cat", categorical_transformer, categorical_features),
+        ]
+    )
+
+    # transform test data
+    test_data_transformed = preprocessor.fit_transform(test_data_used)
+    return test_data_transformed
+
 # linear regression class
 class LR:
-    def __init__(self, X, y) -> None:
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    def __init__(self, train_data) -> None:
+        X, y = preprocess_train_data(train_data)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
         self.lr = LogisticRegression()
-        self.lr.fit(self.X_train, self.y_train)
+        self.lr.fit(X_train, y_train)
 
     def predict(self, X):
         return self.lr.predict(X)
+
+    def evalualte(self, test_data, out_file):
+        test_data_transformed = preprocess_test_data(test_data)
+        y_pred = self.lr.predict(test_data_transformed)
+        print(f"shape of y_pred: {y_pred.shape}")
+        test_data["Survived"] = y_pred
+        result = pd.DataFrame({"PassengerId": test_data['PassengerId'], "Survived": y_pred})
+
+        # right result to a .csv file
+        result.to_csv(out_file, index=False)
+
+# random forest classifier class
+class RFC:
+    def __init__(self, train_data) -> None:
+        X, y = preprocess_train_data(train_data)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+        # find best n_estimators
+        n = range(1,100)
+        results = np.array([])
+        for i in n:
+            rfc_temp = RandomForestClassifier(n_estimators=i)
+            rfc_temp.fit(X_train, y_train)
+            results = np.append(results, rfc_temp.score(X_test, y_test))
+
+        best_estimator = results.argmax()
+        self.rfc = RandomForestClassifier(n_estimators=best_estimator)
+        self.rfc.fit(X_train, y_train)
+
+    def predict(self, X):
+        return self.rfc.predict(X)
+
+    def evaluate(self, test_data, out_file):
+        test_data_transformed = preprocess_test_data(test_data)
+        y_pred = self.rfc.predict(test_data_transformed)
+        print(f"shape of y_pred: {y_pred.shape}")
+        test_data["Survived"] = y_pred
+        result = pd.DataFrame({"PassengerId": test_data['PassengerId'], "Survived": y_pred})
+
+        # right result to a .csv file
+        result.to_csv(out_file, index=False)
 
 # load train and test data
 train_data = load_data("data/train.csv")
 test_data = load_data("data/test.csv")
 
-# preprocess test data, should be turned into a function
-test_data_used = test_data.loc[:,['Pclass', 'Sex', 'Age', 'Fare', 'Embarked']]
+# lr = LR(train_data)
+# lr.evalualte(test_data, "lr_result.csv")
 
-# fill NaN values in test data
-test_data_used['Age'] = test_data_used['Age'].fillna(test_data_used['Age'].mean())
-test_data_used['Fare'] = test_data_used['Fare'].fillna(test_data_used['Fare'].mean())
-
-# scale numeric features
-numeric_features = ['Age', 'Fare']
-numeric_transformer = Pipeline(
-    steps=[('scaler', StandardScaler())]
-)
-
-# one hot encode categorical features
-categorical_features = ['Pclass', 'Sex', 'Embarked']
-categorical_transformer = Pipeline(
-    steps=[("encoder", OneHotEncoder(handle_unknown='ignore'))]
-)
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ("num", numeric_transformer, numeric_features),
-        ("cat", categorical_transformer, categorical_features),
-    ]
-)
-
-# transform test data
-test_data_transformed = preprocessor.fit_transform(test_data_used)
-
-X_train, y_train = preprocess(train_data)
-lr = LR(X_train, y_train)
-
-y_pred = lr.predict(test_data_transformed)
-
-print(f"type of test_data: {type(test_data)}")
-print(f"type of test_data_used: {type(test_data_used)}")
-print(f"type of test_data_transformed: {type(test_data_transformed)}")
-print(f"type of y_pred: {type(y_pred)}")
-print(f"shape of y_pred: {y_pred.shape}")
-
-test_data["Survived"] = y_pred
-
-result = pd.DataFrame({"PassengerId": test_data['PassengerId'], "Survived": y_pred})
-
-# right result to a .csv file
-result.to_csv("lr_result.csv", index=False)
+rfc = RFC(train_data)
+rfc.evaluate(test_data, "rfc_result.csv")
